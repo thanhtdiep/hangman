8e80baff1a1a027e6cbb9ed715c09258eafa38b6
Thanh Diep <thanhtdiep98@gmail.com>
2022-11-12 14:59:22 +0700

    [UPDATE] Added header

--- components/Key.tsx
+++ components/Key.tsx
@@ -4,13 +4,14 @@ interface Props {
     title: string,
     className?: string,
     disabled?: boolean,
+    logo?: boolean,
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
-const Key: FC<Props> = ({ title, className, disabled, onClick }) => {
+const Key: FC<Props> = ({ title, className, disabled, logo, onClick }) => {
 
     return (
         <button
-            className={`flex text-white disabled:cursor-default disabled:opacity-50 uppercase mr-2 mb-2 rounded-md justify-center items-center cursor-pointer w-14 h-14 text-xl border-2 border-white ${className}`} disabled={disabled} onClick={onClick}>
+            className={`flex text-white disabled:cursor-default disabled:opacity-50 uppercase mr-2 mb-2 rounded-md justify-center items-center cursor-pointer ${logo ? 'w-7 h-7 text-sm': 'w-14 h-14 text-xl'}  border-2 border-white ${className}`} disabled={disabled} onClick={onClick}>
             {title}
         </button>
     )
