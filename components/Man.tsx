59b7c0b32daa999e489a7f8efd35fb8d62c40edb
Thanh Diep <bennythanh280198@gmail.com>
2023-05-06 14:49:05 +0700

    Updated Man comp

--- components/Man.tsx
+++ components/Man.tsx
@@ -1,156 +1,152 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
+import { useWindowSize } from "@/hooks/use-window-size";
+import { BREAKPOINT } from "@/helpers/utils/data";
+import { Size, ManProps } from "@/types";
 
-interface Props {
-    lives: number,
-    winSize: Size,
-    className?: string,
-    small?: boolean
-}
+const Man: FC<ManProps> = ({ lives, className, small }) => {
+  const winSize: Size = useWindowSize();
 
-interface Size {
-    width: number,
-    height: number
-}
-
-const Man: FC<Props> = ({ winSize, lives, className, small }) => {
-    const BREAKPOINT = 1028;
-    const SCALE = !small ? winSize.width < BREAKPOINT ? .25 : .5 : winSize.width < BREAKPOINT ? .15 : .3
-    //  resize when on phone
-    return (
-        <svg className={` ${className}`}
-            width={500 * SCALE}
-            height={500 * SCALE}
-            viewBox={`0 0 ${500 * SCALE} ${500 * SCALE}`}
-        >
-            {/* base */}
-            {lives <= 7 && <>
-
-                <line
-                    x1="0"
-                    y1={500 * SCALE}
-                    x2={200 * SCALE}
-                    y2={500 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-                <line
-                    x1="0"
-                    y1={500 * SCALE}
-                    x2={100 * SCALE}
-                    y2={400 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-                <line
-                    x1={100 * SCALE}
-                    y1={400 * SCALE}
-                    x2={200 * SCALE}
-                    y2={500 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-                <line
-                    x1={100 * SCALE}
-                    y1={400 * SCALE}
-                    x2={100 * SCALE}
-                    y2="0"
-                    stroke="white"
-                    strokeWidth={3}
-                />
-            </>
-            }
-            {/* hook */}
-            {lives <= 6 &&
-                <>
-                    <line
-                        x1={100 * SCALE}
-                        y1="0"
-                        x2={250 * SCALE}
-                        y2="0"
-                        stroke="white"
-                        strokeWidth={3}
-                    />
-                    <line
-                        x1={250 * SCALE}
-                        y1="0"
-                        x2={250 * SCALE}
-                        y2={50 * SCALE}
-                        stroke="white"
-                        strokeWidth={3}
-                    />
-                </>
-            }
-            {/* head */}
-            {
-                lives <= 5 &&
-                <circle
-                    cx={250 * SCALE}
-                    cy={90 * SCALE}
-                    r={40 * SCALE}
-                    stroke="white"
-                    fill='transparent'
-                    strokeWidth={3}
-                />
-            }
-            {/* body */}
-            {lives <= 4 &&
-                <line
-                    x1={250 * SCALE}
-                    y1={130 * SCALE}
-                    x2={250 * SCALE}
-                    y2={300 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-            }
-            {/* arm l */}
-            {lives <= 3 &&
-                <line
-                    x1={200 * SCALE}
-                    y1={130 * SCALE}
-                    x2={250 * SCALE}
-                    y2={180 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-            }
-            {/* arm r */}
-            {lives <= 2 &&
-                <line
-                    x1={300 * SCALE}
-                    y1={130 * SCALE}
-                    x2={250 * SCALE}
-                    y2={180 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-            }
-            {/* leg l */}
-            {lives <= 1 &&
-                <line
-                    x1={250 * SCALE}
-                    y1={300 * SCALE}
-                    x2={200 * SCALE}
-                    y2={350 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-            }
-            {/* leg r */}
-            {lives <= 0 &&
-                <line
-                    x1={250 * SCALE}
-                    y1={300 * SCALE}
-                    x2={300 * SCALE}
-                    y2={350 * SCALE}
-                    stroke="white"
-                    strokeWidth={3}
-                />
-
-            }
-
-        </svg>
-    )
-}
+  const SCALE = !small
+    ? winSize.width < BREAKPOINT
+      ? 0.25
+      : 0.5
+    : winSize.width < BREAKPOINT
+    ? 0.15
+    : 0.3;
+  //  resize when on phone
+  return (
+    <svg
+      className={`${className}`}
+      width={500 * SCALE}
+      height={500 * SCALE}
+      viewBox={`0 0 ${500 * SCALE} ${500 * SCALE}`}
+    >
+      {/* base */}
+      {lives <= 7 && (
+        <>
+          <line
+            x1="0"
+            y1={500 * SCALE}
+            x2={200 * SCALE}
+            y2={500 * SCALE}
+            stroke="white"
+            strokeWidth={3}
+          />
+          <line
+            x1="0"
+            y1={500 * SCALE}
+            x2={100 * SCALE}
+            y2={400 * SCALE}
+            stroke="white"
+            strokeWidth={3}
+          />
+          <line
+            x1={100 * SCALE}
+            y1={400 * SCALE}
+            x2={200 * SCALE}
+            y2={500 * SCALE}
+            stroke="white"
+            strokeWidth={3}
+          />
+          <line
+            x1={100 * SCALE}
+            y1={400 * SCALE}
+            x2={100 * SCALE}
+            y2="0"
+            stroke="white"
+            strokeWidth={3}
+          />
+        </>
+      )}
+      {/* hook */}
+      {lives <= 6 && (
+        <>
+          <line
+            x1={100 * SCALE}
+            y1="0"
+            x2={250 * SCALE}
+            y2="0"
+            stroke="white"
+            strokeWidth={3}
+          />
+          <line
+            x1={250 * SCALE}
+            y1="0"
+            x2={250 * SCALE}
+            y2={50 * SCALE}
+            stroke="white"
+            strokeWidth={3}
+          />
+        </>
+      )}
+      {/* head */}
+      {lives <= 5 && (
+        <circle
+          cx={250 * SCALE}
+          cy={90 * SCALE}
+          r={40 * SCALE}
+          stroke="white"
+          fill="transparent"
+          strokeWidth={3}
+        />
+      )}
+      {/* body */}
+      {lives <= 4 && (
+        <line
+          x1={250 * SCALE}
+          y1={130 * SCALE}
+          x2={250 * SCALE}
+          y2={300 * SCALE}
+          stroke="white"
+          strokeWidth={3}
+        />
+      )}
+      {/* arm l */}
+      {lives <= 3 && (
+        <line
+          x1={200 * SCALE}
+          y1={130 * SCALE}
+          x2={250 * SCALE}
+          y2={180 * SCALE}
+          stroke="white"
+          strokeWidth={3}
+        />
+      )}
+      {/* arm r */}
+      {lives <= 2 && (
+        <line
+          x1={300 * SCALE}
+          y1={130 * SCALE}
+          x2={250 * SCALE}
+          y2={180 * SCALE}
+          stroke="white"
+          strokeWidth={3}
+        />
+      )}
+      {/* leg l */}
+      {lives <= 1 && (
+        <line
+          x1={250 * SCALE}
+          y1={300 * SCALE}
+          x2={200 * SCALE}
+          y2={350 * SCALE}
+          stroke="white"
+          strokeWidth={3}
+        />
+      )}
+      {/* leg r */}
+      {lives <= 0 && (
+        <line
+          x1={250 * SCALE}
+          y1={300 * SCALE}
+          x2={300 * SCALE}
+          y2={350 * SCALE}
+          stroke="white"
+          strokeWidth={3}
+        />
+      )}
+    </svg>
+  );
+};
 
 export default Man;
